1c245f70c2813833002c370aba42d7f6c65ad197
Crais2905 <slisovets2905@gmail.com>
2026-02-04 23:48:24 +0200

    full warehouse service

--- app/api/warehouse.py
+++ app/api/warehouse.py
@@ -21,107 +21,56 @@ router = APIRouter()
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=WarehousePublic)
 async def create_warehouse(
     data: WarehouseCreate,
-    warehouse_crud: WarehouseCRUD = Depends(WarehouseCRUD),
+    warehouse_service: WarehouseService = Depends(get_warehouse_service),
     session: AsyncSession = Depends(get_session),
     current_user: UserPublic = Depends(require_role(
         UserRole.admin,
         UserRole.manager
     ))
 ):
-    warehouse = await warehouse_crud.get_object_by_unic_field(
-        data.name, Warehouse.name, session
-    )
-
-    if warehouse:
-        raise HTTPException(
-            status_code=status.HTTP_409_CONFLICT,
-            detail="This name already exist"
-        )
-
-    return await warehouse_crud.write_to_db(data, session)
+    return await warehouse_service.create_warehouse(session, data)
 
 
 @router.get("/{warehouse_id}", status_code=status.HTTP_200_OK, response_model=WarehousePublic)
 async def get_warehouse(
     warehouse_id: UUID,
-    warehouse_crud: WarehouseCRUD = Depends(WarehouseCRUD),
+    warehouse_service: WarehouseService = Depends(get_warehouse_service),
     session: AsyncSession = Depends(get_session)
 ):
-    warehouse = await warehouse_crud.get_object_by_unic_field(
-        warehouse_id, Warehouse.id, session
-    )
-
-    if not warehouse:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Warehouse not found"
-        )
-
-    return warehouse
+    return await warehouse_service.get_warehouse(warehouse_id, session)
 
 
 @router.get("/", status_code=status.HTTP_200_OK, response_model=List[WarehousePublic])
 async def get_warehouses(
     offset: int = 0,
     limit: int = 5,
-    warehouse_crud: WarehouseCRUD = Depends(WarehouseCRUD),
+    warehouse_service: WarehouseService = Depends(get_warehouse_service),
     session: AsyncSession = Depends(get_session)
 ):
-    return await warehouse_crud.get_objects(session, offset, limit)
+    return await warehouse_service.get_warehouses(session, offset, limit)
 
 
 @router.patch("/{warehouse_id}", status_code=status.HTTP_200_OK, response_model=WarehousePublic)
 async def update_warehouse(
     warehouse_id: UUID,
     new_data: WarehouseUpdate,
-    warehouse_crud: WarehouseCRUD = Depends(WarehouseCRUD),
+    warehouse_service: WarehouseService = Depends(get_warehouse_service),
     session: AsyncSession = Depends(get_session),
     current_user: UserPublic = Depends(require_role(
         UserRole.admin, UserRole.manager
     ))
 ):
-    warehouse = await warehouse_crud.get_object_by_unic_field(
-        warehouse_id,
-        Warehouse.id,
-        session
-    )
-
-    if not warehouse:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Warehouse not found"
-        )
-
-    if not warehouse.is_active:
-        raise HTTPException(
-            status_code=status.HTTP_409_CONFLICT,
-            detail="Warehouse isn't active"
-        )
-
-    return await warehouse_crud.update_object(warehouse_id, new_data, session)
+    return await warehouse_service.update_warehouse(warehouse_id, session, new_data)
 
 
 @router.patch("/{warehouse_id}/deactivate", status_code=status.HTTP_200_OK, response_model=WarehousePublic)
 async def warehouse_deactivate(
     warehouse_id: UUID,
-    warehouse_crud: WarehouseCRUD = Depends(WarehouseCRUD),
+    warehouse_service: WarehouseService = Depends(get_warehouse_service),
     session: AsyncSession = Depends(get_session),
     current_user: UserPublic = Depends(require_role(UserRole.admin))
 ):
-    warehouse = await warehouse_crud.get_object_by_unic_field(
-        warehouse_id,
-        Warehouse.id,
-        session
-    )
-
-    if not warehouse:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Warehouse not found"
-        )
-
-    await warehouse_crud.deactivate_object(warehouse_id, session)
-    return warehouse
+    return await warehouse_service.deactivate_warehouse(warehouse_id, session)
 
 
 @router.get("/{warehouse_id}/operations", status_code=status.HTTP_200_OK, response_model=List[InventoryOperationsPublic])
--- app/services/warehouse.py
+++ app/services/warehouse.py
@@ -7,12 +7,58 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.crud.inventory_oprations import InventoryOperationsCRUD
 from app.crud.warehouse import WarehouseCRUD
 from app.db.models import Warehouse
+from app.schemas.rout_schemas.warehouse import WarehouseUpdate, WarehouseCreate
 
 
 class WarehouseService:
     def __init__(self, warehouse_crud: WarehouseCRUD):
         self.warehouse_crud: WarehouseCRUD = warehouse_crud
         self.model = Warehouse
+
+    async def get_warehouses(
+            self, session: AsyncSession,
+            offset: int,
+            limit: int
+    ):
+        if offset < 0 or limit < 0:
+            raise HTTPException(
+                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                detail="offset and limit must be greater than 0"
+            )
+
+        return await self.warehouse_crud.get_objects(session, offset, limit)
+
+    async def get_warehouse(
+            self, warehouse_id: UUID,
+            session: AsyncSession
+    ):
+        warehouse = await self.warehouse_crud.get_object_by_unic_field(
+            warehouse_id, Warehouse.id, session
+        )
+
+        if not warehouse:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Warehouse not found"
+            )
+
+        return warehouse
+
+    async def create_warehouse(
+        self, session: AsyncSession,
+            data: WarehouseCreate
+    ):
+        warehouse = await self.warehouse_crud.get_object_by_unic_field(
+            data.name, Warehouse.name, session
+        )
+
+        if warehouse:
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail="This name already exist"
+            )
+
+        return await self.warehouse_crud.write_to_db(data, session)
 
     async def get_operations_by_warehouse_id(
         self,
@@ -33,6 +79,50 @@ class WarehouseService:
         return await inventory_operation_crud.get_warehouse_operations(
             warehouse, session, offset, limit, desc_=desc
         )
+    
+    async def deactivate_warehouse(
+        self, warehouse_id: UUID,
+        session: AsyncSession
+    ):
+        warehouse = await self.warehouse_crud.get_object_by_unic_field(
+            warehouse_id,
+            Warehouse.id,
+            session
+        )
+
+        if not warehouse:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Warehouse not found"
+            )
+
+        await self.warehouse_crud.deactivate_object(warehouse_id, session)
+        return warehouse
+
+    async def update_warehouse(
+        self, warehouse_id: UUID,
+        session: AsyncSession,
+        new_data: WarehouseUpdate
+    ):
+        warehouse = await self.warehouse_crud.get_object_by_unic_field(
+            warehouse_id,
+            Warehouse.id,
+            session
+        )
+
+        if not warehouse:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Warehouse not found"
+            )
+
+        if not warehouse.is_active:
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail="Warehouse isn't active"
+            )
+
+        return await self.warehouse_crud.update_object(warehouse_id, new_data, session)
 
 
 def get_warehouse_service(
